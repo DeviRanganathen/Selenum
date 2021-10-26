ad2d1f3047df1fd669487cee4cfdb6bf9b60e2a8
DeviRanganathen <deviranganathen212@gmail.com>
2021-10-26 22:13:06 +0530

    project here

---
+++ MyTask.java
@@ -0,0 +1,23 @@
+package org.task;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MyTask {
+
+	
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("http//www.facebook.com/");
+		driver.getClass();
+		
+		
+		
+	
+
+	}
+
+}
